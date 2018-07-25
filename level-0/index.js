75dec85e207a0e67a60c27094686bd2450586482
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-07-25 16:01:40 +0700

    Level 0 - Show every single name in console

---
+++ level-0/index.js
@@ -0,0 +1,14 @@
+const names = [
+    "Mr. Fahri",
+    "Mr. Abdul",
+    "Mrs. Josephhine",
+    "Mr. Joseph",
+    "Mr. Paul",
+    "Mrs. Paula",
+    "Mr. Hakim"
+];
+
+names.forEach(function(name) {
+    console.log(name);
+});
+  
